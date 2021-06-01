d175e4464488114b7275cdf12788fdd84fd063bf
jas-raw <jasandinoa@gmail.com>
2021-06-01 14:36:36 -0500

    Mejoras en pagina 404

--- src/Components/Error404.jsx
+++ src/Components/Error404.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import '../Static/CSS/Error404.css'
+import logo from '../Static/Icons/logo-004@3x.png'
 
 const Error404 = () => {
 
     document.title = "GIG - 404"
 
   return (
-    <div className="container text-center" id='E404-container'>
-    	<h1 className='display-1 text-center'>404</h1>
-    	<h1 className='display-1'>Pagina no encontrada</h1>
-    	<h2>Sentimos las molestias por favor use la barra de busqueda o vuelva a inicio</h2>
+    <div className="container text-center mt-2" id='E404-container'>
+      <div className='row justify-content-center title-404'>
+        <div className='col-auto'>
+          <img src={logo} alt='gig'/>
+        </div>
+        <div className='col-auto'>
+          <h1 className='display-1 text-center'>-</h1>
+        </div>
+        <div className='col-auto'>
+          <h1 className='display-1 text-center'>404</h1>
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='display-3 header-404'>Parece que te has perdido</h1>
+          <h2 className='body-404' >Puedes usar la barra de busqueda o la navegacion para ubicarte nuevamente</h2>
+        </div>
+      </div>
     </div>
   );
 }
